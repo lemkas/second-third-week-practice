b892d4607d22dac9408ab1e63ee110690b70b8b3
lemov nikita <lemov1980@yandex.ru>
2023-04-04 16:24:33 +0300

    the best drag 1.0

--- src/app/directives/druggable.directive.ts
+++ src/app/directives/druggable.directive.ts
@@ -1,12 +1,6 @@
 import { DOCUMENT } from '@angular/common';
-import {
-  Directive,
-  ElementRef,
-  Inject,
-  OnDestroy,
-  OnInit,
-} from '@angular/core';
-import { fromEvent, Subscription } from 'rxjs';
+import { Directive, ElementRef, Inject, OnInit } from '@angular/core';
+import { fromEvent, Subscription, takeUntil } from 'rxjs';
 
 @Directive({
   selector: '[Draggable]',
@@ -34,19 +28,17 @@ export class DruggableDirective implements OnInit {
 
   initDrug(): void {
     const dragStart = fromEvent<MouseEvent>(this.element, 'mousedown');
-    const dragEnd = fromEvent<MouseEvent>(this.element, 'mouseup');
-    const drag = fromEvent<MouseEvent>(this.element, 'mouseover');
+    const dragEnd = fromEvent<MouseEvent>(this.document, 'mouseup');
+    const drag = fromEvent<MouseEvent>(this.document, 'mouseover');
 
-    let currentX: number,
-      currentY: number,
+    let currentX: number = 0,
+      currentY: number = 0,
       initialX: number,
-      initialY: number,
-      xOffset: number = 0,
-      yOffset: number = 0;
+      initialY: number;
 
     this.dragStartSub = dragStart.subscribe((e: MouseEvent) => {
-      initialX = e.clientX - xOffset;
-      initialY = e.clientY - yOffset;
+      initialX = e.clientX - currentX;
+      initialY = e.clientY - currentY;
 
       if (e.target === this.element) {
         this.isDragging = true;
@@ -68,7 +60,6 @@ export class DruggableDirective implements OnInit {
       initialX = currentX;
       initialY = currentY;
       this.isDragging = false;
-      console.log(this.isDragging);
     });
   }
 }
